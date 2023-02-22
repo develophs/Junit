ece9ee7b1199b75313962b12141a6e8c075d4d48
HanseongBae <bha4388@naver.com>
2023-02-22 21:18:38 +0900

    feat :  Book엔티티 생성 완료

--- src/main/java/com/study/junitproject/domain/Book.java
+++ src/main/java/com/study/junitproject/domain/Book.java
@@ -1,4 +1,41 @@
 package com.study.junitproject.domain;
 
+import lombok.Builder;
+
+import javax.persistence.*;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 20, nullable = false)
+    private String author;
+
+    public Book() {
+    }
+
+    @Builder
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
 }
