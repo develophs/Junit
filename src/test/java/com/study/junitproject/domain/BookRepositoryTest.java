ccfc5de0d23e68a2bd517a674b1280da99b76127
HanseongBae <bha4388@naver.com>
2023-02-23 20:40:42 +0900

    feat : 책등록 테스트 완료

--- src/test/java/com/study/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/com/study/junitproject/domain/BookRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.study.junitproject.domain;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest //DB와 관련된 컴포넌트만 메모리에 로딩, Controller,Service관련 구동X, 구동이 가벼워진다.
 @Slf4j
@@ -19,7 +22,21 @@ public class BookRepositoryTest {
     // 1. 책 등록
     @Test
     public void 책등록_test(){
-        log.info("책등록_test 실행");
+        //given(데이터 준비)
+        String title = "Junit5";
+        String author= "Jake";
+
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+
+        //when(테스트 실행)
+        Book savedBook = bookRepository.save(book);
+
+        //then(검증)
+        assertThat(savedBook.getTitle()).isEqualTo(title);
+        assertThat(savedBook.getAuthor()).isEqualTo(author);
     }
 
     // 2. 책 목록보기
