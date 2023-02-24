4fb0793b5a21bd0019ad183c6469dc56bab70957
HanseongBae <bha4388@naver.com>
2023-02-24 21:07:52 +0900

    feat : select test

--- src/test/java/com/study/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/com/study/junitproject/domain/BookRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.study.junitproject.domain;
 
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -18,6 +21,20 @@ public class BookRepositoryTest {
     //DB쪽 관련 테스트
     //책이 등록 될때, author가 null이다.
     //-> 유효성 검증은 controller에서 하는것.
+
+    // @BeforeAll 테스트전 1번만 발생
+    @BeforeEach // 각 테스트 시작전 발생
+    public void 데이터준비(){
+        String title = "Junit5";
+        String author= "Jake";
+
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+    } //트랜잭션은 데이터준비 + 각각의 테스트 메서드 유지
+
 
     // 1. 책 등록
     @Test
@@ -37,12 +54,37 @@ public class BookRepositoryTest {
         //then(검증)
         assertThat(savedBook.getTitle()).isEqualTo(title);
         assertThat(savedBook.getAuthor()).isEqualTo(author);
+        // 트랜잭션 종료(저장된 데이터를 초기화한다)
     }
-
     // 2. 책 목록보기
+    @Test
+    public void 책목록보기_test(){
+        //given
+        String title = "Junit5";
+        String author= "Jake";
 
+        //when
+        List<Book> books = bookRepository.findAll();
+
+        //then
+        assertThat(books.get(0).getTitle()).isEqualTo(title);
+        assertThat(books.get(0).getAuthor()).isEqualTo(author);
+        assertThat(books.size()).isEqualTo(1);
+    }
     // 3. 책 한건보기
+    @Test
+    public void 책한건보기_test(){
+        //given
+        String title = "Junit5";
+        String author= "Jake";
 
+        //when
+        Book findBook = bookRepository.findById(1L).orElseThrow();
+
+        //then
+        assertThat(findBook.getTitle()).isEqualTo(title);
+        assertThat(findBook.getAuthor()).isEqualTo(author);
+    }
     // 4. 책 수정
 
     // 5. 책 삭제
