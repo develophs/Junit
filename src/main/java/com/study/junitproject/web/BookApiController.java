32373d9f1259431527716c10c3a1d382b4ea1895
HanseongBae <bha4388@naver.com>
2023-03-01 07:47:27 +0900

    feat : NotBlank, Valid, BingdingResult, Map을 이용한 유효성처리

--- src/main/java/com/study/junitproject/web/BookApiController.java
+++ src/main/java/com/study/junitproject/web/BookApiController.java
@@ -5,12 +5,20 @@ import com.study.junitproject.web.dto.response.BookRespDto;
 import com.study.junitproject.web.dto.request.BookSaveReqDto;
 import com.study.junitproject.web.dto.response.CMRespDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class BookApiController {
@@ -19,10 +27,21 @@ public class BookApiController {
 
     // 1.책 등록
     @PostMapping("/api/v1/book")
-    public ResponseEntity<?> saveBook(@RequestBody BookSaveReqDto bookSaveReqDto){
+    public ResponseEntity<?> saveBook(@RequestBody @Valid BookSaveReqDto bookSaveReqDto,
+                                      BindingResult bindingResult){
+        //추후에 AOP처리
+        if(bindingResult.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+            for(FieldError error : bindingResult.getFieldErrors()){
+                errorMap.put(error.getField(),error.getDefaultMessage());
+            }
+            log.info("hasErrors={}",bindingResult.hasErrors());
+            log.info("errorMap={}",errorMap.toString());
+            return new ResponseEntity<>(new CMRespDto(-1, "실패", errorMap.toString()),HttpStatus.BAD_REQUEST); //400
+        }
+
         BookRespDto bookRespDto = bookService.책등록하기(bookSaveReqDto);
-        CMRespDto body = new CMRespDto(1, "성공", bookRespDto);
-        return new ResponseEntity<>(body,HttpStatus.CREATED); //201
+        return new ResponseEntity<>(new CMRespDto(1, "성공", bookRespDto),HttpStatus.CREATED); //201
     }
 
     // 2.책 목록보기
@@ -45,3 +64,4 @@ public class BookApiController {
         return null;
     }
 }
+
--- src/main/java/com/study/junitproject/web/dto/request/BookSaveReqDto.java
+++ src/main/java/com/study/junitproject/web/dto/request/BookSaveReqDto.java
@@ -3,10 +3,19 @@ package com.study.junitproject.web.dto.request;
 import com.study.junitproject.domain.Book;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Setter //Controller에서 Setter가 호출되면서 Dto에 값이 채워짐.
 public class BookSaveReqDto {
 
+    //null, 공백 체크
+    @Size(min=1, max=50)
+    @NotBlank
     private String title;
+
+    @Size(min=2, max=20)
+    @NotBlank
     private String author;
 
     public Book toEntity(){
