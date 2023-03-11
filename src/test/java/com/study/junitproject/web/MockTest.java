2ea86ea07984aeff7e43ba328bc261da80cd8a26
HanseongBae <bha4388@naver.com>
2023-03-11 23:59:33 +0900

    feat : mock test

---
+++ src/test/java/com/study/junitproject/web/MockTest.java
@@ -0,0 +1,28 @@
+package com.study.junitproject.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest
+public class MockTest {
+
+    @Autowired
+    MockMvc mock;
+
+    @Test
+    @DisplayName("mock 테스트 시작")
+    void test() throws Exception {
+        mock.perform(MockMvcRequestBuilders.post("/api/v1/book")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\":\"mockStudy\", \"author\":\"jake\"}"))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
